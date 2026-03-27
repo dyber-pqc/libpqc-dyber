463541974310e4a3ed91b32baf76d39ca1e9face
Zachary Kleckner <zkleckner@dyber.org>
2026-03-27 16:53:26 -0400

    Remove build-wsl/build-asan from repo and add to .gitignore

--- build-asan/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/h/libpqc-dyber
-# Build directory: /mnt/h/libpqc-dyber/build-asan
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
--- build-wsl/CMakeFiles/pqc_static.dir/src/core/sig/mldsa/hint.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/pqc_static.dir/src/core/sig/mldsa/hint.c.o: \
- /mnt/h/libpqc-dyber/src/core/sig/mldsa/hint.c /usr/include/stdc-predef.h
